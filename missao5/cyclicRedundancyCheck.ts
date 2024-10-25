276d7d9a5b75165fac92157dddc9e17f47022e1e
GGTI/LUIZ.FERNANDO <11830452479@sdspe.infra>
2024-10-25 13:53:01 -0300

    missao5 25/10/2024

--- missao5/cyclicRedundancyCheck.ts
+++ missao5/cyclicRedundancyCheck.ts
@@ -1,24 +1,40 @@
-//contar consoantes
-
-function contarConsoantes(frase: string): number {
+function contarConsoantes(frase: string) {
+    let fraseTratada: string = frase.toLowerCase()
     let consoantes: number = 0;
-    //let alfabetoConsoantes: string = "bcdfghjklmnpqrstvwxyz";
-    
-    for(let i=0; i<frase.length; i++) {
-        if (frase[i] !== 'a' || frase[i] !== 'e' || frase[i] !== 'i' || frase[i] !== 'o' || frase[i] !== 'u'){
-            consoantes ++;
+    let valorDecimal: string = "";
+    let alfabetoConsoantes: string = "bcdfghjklmnpqrstvwxyz";
+
+    for (let i = 0; i < fraseTratada.length; i++) {
+        //if (fraseTratada[i] !== 'a' || fraseTratada[i] !== 'e' || fraseTratada[i] !== 'i' || fraseTratada[i] !== 'o' || fraseTratada[i] !== 'u') {
+        if (alfabetoConsoantes.includes(fraseTratada[i])){
+            consoantes++;
+            if (consoantes < 10) {
+                valorDecimal = "0" + String(consoantes);
+            }
         }
     }
-    return consoantes;
+    return valorDecimal
 }
+const quantidadeConsosnates: string = contarConsoantes("Antonia")
+console.log(quantidadeConsosnates)
 
-function codificadorCRC (palavra: string){
-    let vogais: number = 0;
+function contadorVogais(palavra: string) {
+    let palavraTratada: string = palavra.toLowerCase() 
+    let vogais: string = "aeiou";
+    let valorBinario: string = "";
 
-    for(let i=0; i<palavra.length; i++){
-        if (palavra[i] === 'a' || palavra[i] === 'e' || palavra[i] === "i" || palavra[i] === "o" || palavra[i] === "u"){
-            vogais ++;
+    for (let i = 0; i < vogais.length; i++) {
+        if (palavraTratada.includes(vogais[i])) {
+            valorBinario += "1";
+        } else {
+            valorBinario += "0";
         }
     }
+    return valorBinario;
 }
-codificadorCRC("Marlon")
+const valorBinario: string = contadorVogais("Antonia");
+console.log(valorBinario);
+
+const valorDecimal: number = parseInt(valorBinario, 2);
+
+console.log(valorDecimal + quantidadeConsosnates)
